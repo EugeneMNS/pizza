8b47d31470fa4f5126dea1611504c3aac95c6b76
Eugene Novik <evgen.str4@gmail.com>
2022-06-16 12:25:29 +0300

    feat: sorting implemented

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,21 +1,18 @@
 import React, {useState} from "react";
 
-export function Categories() {
-    const [activeIndex, setActiveIndex] = useState(0)
-    const categories = ['Все', 'Мячные', 'Вегетариансткая', 'Гриль', 'Острые', 'Закрытые']
-    const onClickCategory = (index) => {
-        setActiveIndex(index)
-    }
+export function Categories({value, onChangeCategory}) {
+    const categories = ['Все', 'Мясные', 'Вегетариансткая', 'Гриль', 'Острые', 'Закрытые']
+
 
 
     return (
         <div className="categories">
             <ul>
-                {categories.map((value, index) => (
-                    <li key={index}
-                        onClick={() => onClickCategory(index)}
-                        className={activeIndex === index ? 'active' : ''}>
-                        {value}
+                {categories.map((categoryName, i) => (
+                    <li key={i}
+                        onClick={() => onChangeCategory(i)}
+                        className={value === i ? 'active' : ''}>
+                        {categoryName}
                     </li>
                 ))}
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,24 +9,35 @@ import {PizzaBlock} from "../components/PizzaBlock/PizzaBlock";
 const Home = () => {
     const [items, setItems] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
+    const [categoryId, setCategoryId] = useState(0)
+    const [sortType, setSortType] = useState({
+        name: 'популярности',
+        sortProperty: 'Rating'
+    })
+
 
     useEffect(() => {
-        fetch('https://62a97197ec36bf40bdb79673.mockapi.io/items')
-            .then((res) => {
-                return res.json();
-            })
+        setIsLoading(true)
+
+        const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
+        const sortBy =sortType.sortProperty.replace('-', '')
+        const  category =categoryId > 0 ? `category=${categoryId}` : ''
+
+        fetch(
+            `https://62a97197ec36bf40bdb79673.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`)
+            .then((res) => {return res.json()})
             .then((arr) => {
                 setItems(arr)
                 setIsLoading(false)
             })
         window.scroll(0,0)
-    }, [])
+    }, [categoryId, sortType])
 
     return (
         <div className="container">
             <div className="content__top">
-                <Categories/>
-                <Sort/>
+                <Categories value={categoryId} onChangeCategory={(i)=>setCategoryId(i)}/>
+                <Sort value={sortType} onChangeSort={(i)=>setSortType(i)}/>
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
