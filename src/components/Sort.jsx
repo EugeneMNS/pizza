ee11e76a5e69495fa4945083c7ce30f6f5c6fa59
Eugene Novik <evgen.str4@gmail.com>
2022-06-21 18:29:50 +0300

    feat: hiding sort popup

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {setSort} from "../redux/slices/filterSlice";
 
@@ -15,6 +15,7 @@ export const sortList = [
 export function  Sort(){
     const dispatch = useDispatch();
     const sort = useSelector(state => state.filter.sort)
+    const sortRef = useRef()
 
     const [open, setOpen]= useState(false)
 
@@ -26,8 +27,21 @@ export function  Sort(){
     }
 
 
+    useEffect(()=>{
+        const handleClickOutside = (event) =>{
+            if(!event.path.includes(sortRef.current)){
+                setOpen(false)
+            }
+        }
+
+        document.body.addEventListener('click', handleClickOutside)
+
+        return ()=>document.body.removeEventListener('click', handleClickOutside)
+    }, [])
+
+
     return(
-        <div className="sort">
+        <div ref={sortRef} className="sort">
             <div className="sort__label">
                 <svg
                     width="10"
