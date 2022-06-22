98dcdd7057d3341b5e8fa358f68590ff42e98f27
Eugene Novik <evgen.str4@gmail.com>
2022-06-22 18:34:12 +0300

    feat: adding items to cart

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,9 +1,30 @@
 import React, {useState} from "react";
+import {useSelector, useDispatch} from "react-redux";
+import {addItem} from "../../redux/slices/cartSlice";
 
-export function PizzaBlock({title, price, imageUrl, sizes, types}) {
+
+const typeNames = ['тонкое', 'традиционное']
+
+
+export function PizzaBlock({id, title, price, imageUrl, sizes, types}) {
+    const dispatch = useDispatch()
+    const cartItem = useSelector(state => state.cart.items.find(obj => obj.id === id))
     const [activeType, setActiveType] = useState(0)
     const [activeSize, setActiveSize] = useState(0)
-    const typeNames = ['тонкое', 'традиционное']
+
+    const addedCount = cartItem ? cartItem.count :0
+
+    const onClickAdd = () =>{
+        const item ={
+            id,
+            title,
+            price,
+            imageUrl,
+            type: typeNames[activeType],
+            size: activeSize,
+        }
+        dispatch(addItem(item))
+    }
 
     return (
         <div className='pizza-block-wrapper'>
@@ -38,7 +59,7 @@ export function PizzaBlock({title, price, imageUrl, sizes, types}) {
                     ))}
                 </ul>
             </div>
-            <div className="pizza-block__bottom">
+            <div onClick={onClickAdd} className="pizza-block__bottom">
                 <div className="pizza-block__price">от {price} ₽</div>
                 <button className="button button--outline button--add">
                     <svg
@@ -54,7 +75,7 @@ export function PizzaBlock({title, price, imageUrl, sizes, types}) {
                         />
                     </svg>
                     <span>Добавить</span>
-                    <i>0</i>
+                    {addedCount > 0 && <i>{addedCount}</i>}
                 </button>
             </div>
         </div>
