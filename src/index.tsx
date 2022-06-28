3419ea879fc21101b4e76f3ed8bce77e1d2d580f
Eugene Novik <evgen.str4@gmail.com>
2022-06-28 12:45:44 +0300

    refactor: start migration on TypeScript. Refactor: App, FullPizza, index.tsx & add tsconfig.json

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,19 @@ import {BrowserRouter} from "react-router-dom";
 import {store} from "./redux/store";
 import {Provider} from 'react-redux'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
-    </Provider>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+    const root = ReactDOM.createRoot(rootElement)
+
+    root.render(
+        <Provider store={store}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
+    );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/FullPizza.tsx
+++ src/pages/FullPizza.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import axios from 'axios';
-import { useParams, useNavigate } from 'react-router-dom';
+import {useParams, useNavigate} from 'react-router-dom';
 
-const FullPizza = () => {
-    const [pizza, setPizza] = React.useState();
-    const { id } = useParams();
+const FullPizza: React.FC = () => {
+    const [pizza, setPizza] = React.useState<{
+        imageUrl: string
+        title: string
+        price: number
+    }>();
+    const {id} = useParams();
     const navigate = useNavigate();
 
     React.useEffect(() => {
         async function fetchPizza() {
             try {
-                const { data } = await axios.get('https://626d16545267c14d5677d9c2.mockapi.io/items/' + id);
+                const {data} = await axios.get('https://626d16545267c14d5677d9c2.mockapi.io/items/' + id);
                 setPizza(data);
             } catch (error) {
                 alert('Ошибка при получении пиццы!');
@@ -22,12 +26,12 @@ const FullPizza = () => {
     }, []);
 
     if (!pizza) {
-        return 'Загрузка...';
+        return <>Загрузка...</>;
     }
 
     return (
         <div className="container">
-            <img src={pizza.imageUrl} />
+            <img src={pizza.imageUrl}/>
             <h2>{pizza.title}</h2>
             <h4>{pizza.price} ₽</h4>
         </div>
